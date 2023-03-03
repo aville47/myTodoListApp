a898c097c2574379241475808ae6a746539eb7de
aville <alexia.ville@sogeti.com>
2023-03-03 10:58:32 +0100

    initialisation back

---
+++ backTodoApp/src/main/java/aville/backTodoApp/BackTodoAppApplication.java
@@ -0,0 +1,13 @@
+package aville.backTodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTodoAppApplication.class, args);
+	}
+
+}
---
+++ backTodoApp/src/test/java/aville/backTodoApp/BackTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package aville.backTodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
